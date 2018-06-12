8b17b7df3e32bf7a6f5db1fcb521d4a247f01023
Maverick <embrymaverick@gmail.com>
2018-06-11 17:59:14 -0700

    Styling and additives

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var exphbs = require("express-handlebars");
 var path = require("path");
 var app = express();
 
+var Note = require("./models/Note.js");
+var Article = require("./models/article.js");
+
 
 
 app.engine("handlebars", exphbs({
